c60c20d0d3ce006bc9f7db50998f349133b3d6c7
Andrejulian21 <julian21arteaga@gmail.com>
2026-03-25 17:08:18 -0500

    a
    a

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ with tabs[2]:
         a = st.number_input("Clave privada A", step=1)
         b = st.number_input("Clave privada B", step=1)
 
-        if st.button("Calcular2"):
+        if st.button("Calcular"):
             res, pasos = diffie_hellman(p, g, a, b)
 
     # =========================
